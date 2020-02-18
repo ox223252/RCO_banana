9629a97ecb9b4debc39bbc262d3ee4133ec928a3
ox223252 <23252@laposte.net>
2020-02-18 22:30:08 +0100

    Create file taht directly manage json
    Modifications qui seront validées :
    	nouveau fichier : src/actionExtract.c
    	nouveau fichier : src/actionExtract.h

---
+++ src/actionExtract.c
@@ -0,0 +1,320 @@
+#include "actionExtract.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include "lib/log/log.h"
+
+/// \retrun 0 : valid id found
+///     -1 : no step remaining
+///     X : line error
+int getStepId ( const json_el * const data, uint32_t * const stepId )
+{
+	if ( !data ||
+		!stepId )
+	{
+		errno = EINVAL;
+		logDebug ( "\n" );
+		return ( __LINE__ );
+	}
+
+	uint32_t * strategieId = 0;
+	JSON_TYPE type = jT( undefined );
+
+	if ( !jsonGetRecursive ( data, 0, "Strategie", (void*)&strategieId, &type ) )
+	{ // no object found
+		logDebug ( "\n" );
+		return ( __LINE__ );
+	}
+
+	if ( type != jT( array ) )
+	{ // strategie is not an array
+		logDebug ( "\n" );
+		return ( __LINE__ );
+	}
+
+	// verify if index is valid and if step associated is an object
+	if ( *stepId < data[ *strategieId ].length )
+	{
+		if ( data[ *strategieId ].type[ *stepId ] == jT( obj ) )
+		{
+			*stepId = *(uint32_t*)data[ *strategieId ].value[ *stepId ];
+			return ( 0 );
+		}
+	}
+
+	return ( -1 );
+}
+
+// TODO : fonction qui calcule l'ordre des etapes
+int getNextStepId ( const json_el * const data, uint32_t * stepId )
+{
+	if ( !data ||
+		!stepId )
+	{
+		errno = EINVAL;
+		logDebug ( "\n" );
+		return ( __LINE__ );
+	}
+
+	uint32_t * strategieId = 0;
+	JSON_TYPE type = jT( undefined );
+
+	if ( !jsonGetRecursive ( data, 0, "Strategie", (void*)&strategieId, &type ) )
+	{ // no object found
+		logDebug ( "\n" );
+		return ( __LINE__ );
+	}
+
+	if ( type != jT( array ) )
+	{ // strategie is not an array
+		logDebug ( "\n" );
+		return ( __LINE__ );
+	}
+
+	// verify if index is valid and if step associated is an object
+	if ( *stepId < data[ *strategieId ].length )
+	{
+		if ( data[ *strategieId ].type[ *stepId ] == jT( obj ) )
+		{
+			*stepId = *(uint32_t*)data[ *strategieId ].value[ *stepId ];
+			return ( 0 );
+		}
+	}
+
+	return ( -1 );
+}
+
+int getActionId ( const json_el * const data, const uint32_t stepId, uint32_t * const actionId )
+{
+	if ( !data ||
+		!actionId )
+	{
+		errno = EINVAL;
+		return ( __LINE__ );
+	}
+
+	JSON_TYPE type = jT( undefined );
+	uint32_t * sequenceId = NULL;
+
+	if ( ( sequenceId == NULL ) &&
+		!jsonGetRecursive ( data, stepId, "arraySequence", (void*)&sequenceId, &type ) )
+	{ // no object found
+		logDebug ( "\n" );
+		return ( __LINE__ );
+	}
+
+	if ( type != jT( array ) )
+	{ // strategie is not an array
+		logDebug ( "\n" );
+		return ( __LINE__ );
+	}
+
+	// verify if index is valid and if action associated is an object
+	if ( *actionId < data[ *sequenceId ].length )
+	{
+		if ( data[ *sequenceId ].type[ *actionId ] == jT( obj ) )
+		{
+			*actionId = *(uint32_t*)data[ *sequenceId ].value[ *actionId ];
+			return ( 0 );
+		}
+	}
+
+	return ( -1 );
+}
+
+int getNextActions ( const json_el * const data, const uint32_t stepId,  const uint32_t actionId,
+	uint32_t ** const out, uint32_t * const length, bool timeout )
+{
+	if ( !data ||
+		!out ||
+		!length )
+	{
+		errno = EINVAL;
+		logDebug ( "\n" ); // c'est pas un tableau
+		return ( __LINE__ );
+	}
+
+	char search[16] = "arrayGirl";
+
+	if ( timeout )
+	{
+		sprintf ( search, "arrayTimeout" ); 
+	}
+
+	JSON_TYPE type;
+	uint32_t *fils = NULL;
+
+	if ( !jsonGet ( data, actionId, search, (void**)&fils, &type ) )
+	{ // pas d'element'
+		return ( -1 );
+	}
+
+	if ( type != jT( array ) )
+	{
+		logDebug ( "%d\n", type ); // c'est pas un tableau
+		return ( __LINE__ );
+	}
+
+	if ( data[ *fils ].length == 0 )
+	{ // tableau vide
+		return ( -1 );
+	}
+
+	void * tmp = malloc ( sizeof ( uint32_t ) * data[ *fils ].length );
+	if ( !tmp )
+	{
+		logDebug ( "\n" ); // problème de malloc
+		return ( __LINE__ );
+	}
+
+	*out = tmp;
+	*length = data[ *fils ].length;
+
+	if ( !timeout )
+	{
+		sprintf ( search, "indiceFille" ); 
+	}
+	else
+	{
+		sprintf ( search, "indiceTimeout" ); 
+	}
+
+	for ( int i = 0; i < data[ *fils ].length; i++ )
+	{
+		double *id = NULL;
+
+		if( !jsonGet ( data, *(uint32_t*)data[ *fils ].value[ i ], search, &id, &type ) )
+		{
+			logDebug ( "\n" ); // error il manque l'action suivante
+			return ( __LINE__ );
+		}
+		if ( type != jT( double ) )
+		{
+			logDebug ( "\n" ); // error la donnée n'est pas un nombre
+			return ( __LINE__ );
+		}
+		(*out)[ i ] = (uint32_t)*id;
+	}
+
+	return ( 0 );
+}
+
+int getActionParams ( const json_el * const data, const uint32_t actionId, json_el ** out, uint32_t * outLength )
+{
+
+	if ( !data )
+	{ // if data provided then clean all static vars
+		errno = EINVAL;
+		return ( __LINE__ );
+	}
+
+	uint32_t * paramArrayId = NULL;
+	JSON_TYPE type = jT( undefined );
+
+	if ( !jsonGetRecursive ( data, actionId, "arrayParam", (void*)&paramArrayId, &type ) )
+	{ // no object found
+		logDebug ( "\n" );
+		return ( __LINE__ );
+	}
+
+	if ( type != jT( array ) )
+	{ // strategie is not an array
+		logDebug ( "%d %d\n", *paramArrayId, type );
+		return ( __LINE__ );
+	}
+
+	jsonParseString ( "{}", out, outLength );
+
+	for ( uint32_t k = 0; k < data[ *paramArrayId ].length; k++ )
+	{
+		uint32_t paramId = *(uint32_t*)data[ *paramArrayId ].value[ k ];
+
+		char * key = NULL;
+		char * value = NULL;
+		jsonGetRecursive ( data, paramId, "nomParam", (void*)&key, &type );
+		if ( type != jT( str ) )
+		{ // key invalid
+			continue;
+		}
+
+		jsonGetRecursive ( data, paramId, "defaultValue", (void*)&value, &type );
+		jsonSet ( *out, 0, key, value, type );
+	}
+
+	return ( 0 );
+}
+
+int __attribute__((weak)) main ( void )
+{
+	json_el *data = NULL;
+	uint32_t dataLength = 0;
+
+	if ( jsonParseFile ( "res/export.json", &data, &dataLength ) )
+	{
+		logDebug ( "\n" );
+		return ( __LINE__ );
+	}
+
+	// id of the step in the sequence array
+	uint32_t stepIndex = 0;
+	do 
+	{
+		// id of the step in main data memory array
+		uint32_t stepId = stepIndex;
+
+		if ( getStepId ( data, &stepId ) )
+		{
+			logDebug ( "\n" );
+			break;
+		}
+		
+		printf ( "etape %d\n", stepId );
+
+		uint32_t actionIndex = 0;
+		do
+		{
+			uint32_t actionId = actionIndex;
+
+			if ( getActionId ( data, stepId, &actionId ) )
+			{
+				logDebug ( "\n" );
+				break;
+			}
+
+			char * name = NULL;
+			jsonGet ( data, actionId, "nomAction", (void**)&name, NULL );
+			printf ( "    action %d : %s\n", actionId, name );
+
+			json_el * params = NULL;
+			uint32_t length = 0;
+			if ( getActionParams ( data, actionId, &params, &length ) )
+			{
+				printf ( "\tno parameters found\n" );
+			}
+			else
+			{
+				printf ( "\tparameters :\n" );
+				if ( params->length == 0 )
+				{
+					printf ( "\tarray empty\n" );
+				}
+				else
+				{
+					printf("\t{\n" );
+					jsonPrint ( params, 0, 1 );
+					printf("\t}\n" );
+				}
+				jsonFree ( &params, length );
+			}
+			actionIndex++;
+		}
+		while ( true );
+
+		stepIndex++;
+	}
+	while ( true );
+
+	jsonFree ( &data, dataLength );
+
+	return ( 0 );
+}
---
+++ src/actionExtract.h
@@ -0,0 +1,67 @@
+#ifndef __ACTIONEXTRACT_H__
+#define __ACTIONEXTRACT_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "lib/jsonParser/jsonParser.h"
+
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int getStepId ( const json_el * const data, uint32_t * const stepId );
+/// \prama[in] data : json element where you search
+/// \param[in/out] stepId : step number in the strategie main array
+/// \brief this function will convert the index in the strategie array into an 
+///     id in the data array, this id should be used to feed getActionId func.
+/// \note stepId is used as input and output
+/// \return 0 : ok, -1 : no data remaining, else error see errno
+////////////////////////////////////////////////////////////////////////////////
+int getStepId ( const json_el * const data, uint32_t * const stepId );
+
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int getActionId ( const json_el * const data, const uint32_t stepId, 
+///     uint32_t * const actionId );
+/// \prama[in] data : json element where you search
+/// \param[in] stepId : step index in the main data array
+/// \param[in/out] actionId : action number in the step main array
+/// \brief this function will convert the index in the step array into an id in
+///     the data array, this id should be used to feed getActionId func.
+/// \note actionId is used as input and output
+/// \return 0 : ok, -1 : no data remaining, else error see errno
+////////////////////////////////////////////////////////////////////////////////
+int getActionId ( const json_el * const data, const uint32_t stepId, 
+	uint32_t * const actionId );
+
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int getNextActions ( const json_el * const data, const uint32_t stepId,  
+///     const uint32_t actionId, uint32_t ** const out, uint32_t * const length, 
+///     bool timeout );
+/// \param[in] data : json element where you search
+/// \param[in] stepId : step index in the main data array
+/// \param[in] actionId : action index in the data main array
+/// \param[out] out : table of id of next els
+/// \param[out] length : size of table in elements
+/// \param[in] timeout : search for timeout or not
+/// \brief return a table of id  of the nexts actions
+/// \note don't forget to free out table after use
+/// \return 0 : ok, -1 : no data remaining, else error see errno
+////////////////////////////////////////////////////////////////////////////////
+int getNextActions ( const json_el * const data, const uint32_t stepId,  
+	const uint32_t actionId, uint32_t ** const out, uint32_t * const length, 
+	bool timeout );
+
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int getActionParams ( const json_el * const data, const uint32_t action,
+///     json_el ** out, uint32_t * outLength );
+/// \prama[in] data : json element where you search
+/// \param[in] actionId : action index in the data main array
+/// \param[out] out : json object containing parameters of action
+/// \param[out] outLength : nb of element in ac
+/// \brief this function will extract params form action associated to actionId
+///     and return JSON
+/// \note *out should be null, else you will probably have memory leak
+/// \return 0 : ok, -1 : no data remaining, else error see errno
+////////////////////////////////////////////////////////////////////////////////
+int getActionParams ( const json_el * const data, const uint32_t actionId, 
+	json_el ** out, uint32_t * outLength );
+
+#endif
